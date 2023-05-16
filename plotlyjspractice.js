e664b30811285f28aaaba0d3643772a9da7db5aa
fgar13 <94147771+fgar13@users.noreply.github.com>
2023-05-15 23:13:06 -0400

    Successfully visualized a scatter plot

---
+++ plotlyjspractice.js
@@ -0,0 +1,19 @@
+function plot(){
+  var trace1 = {
+      x: [1, 2, 3, 4],
+      y: [10, 15, 13, 17],
+      type: 'scatter'
+      };
+      
+      var trace2 = {
+      x: [1, 2, 3, 4],
+      y: [16, 5, 11, 9],
+      type: 'scatter'
+      };
+      
+      var data = [trace1, trace2];
+      myDiv = 'myDiv'
+      Plotly.newPlot(myDiv, data);
+      
+}
+plot()
